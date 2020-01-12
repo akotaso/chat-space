ba09cf97545b7827e262ce28a307d3b4fa3326ac
akotaso <oimo.moimo@gmail.com>
2020-01-12 17:08:36 +0900

    CreateAutomaticUpdating

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,35 +1,32 @@
 $(function(){
-  function buildHTML(message){
-    if (message.image){
-      var html =  `<div class="message">
-                    <div class="message__name">
-                      ${message.user_name}
-                    </div>
-                    <div class="message__date">
-                      ${message.created_at}
-                    </div>
-                    <p class="message__text">
-                      ${message.body}
-                    </p>
-                    <img src=${message.image}>
-                  </div>`
-                  return html;
-    } else {
+  var buildHTML = function(message){
+    var DuplicationHTML = `<div class="message" data-message-id="${message.id}" >
+                            <div class="message__name">
+                              ${message.user_name}
+                            </div>
+                            <div class="message__date">
+                              ${message.created_at}
+                          </div>`
+
+    if (message.body && message.image) {
+      var html = `${DuplicationHTML}
+                   <p class="message__text">
+                     ${message.body}
+                   </p>
+                   <img src=${message.image}>`
+    } else if (message.body) {
       var html = 
-      `<div class="message">
-        <div class="message__name">
-          ${message.user_name}
-        </div>
-        <div class="message__date">
-          ${message.created_at}
-        </div>
+      `${DuplicationHTML}
         <p class="message__text">
           ${message.body}
-        </p>
-      </div>`
-      return html;
+        </p>`
+    } else if (message.image) {
+      var html =
+      `${DuplicationHTML}
+        <img src=${message.image}>`
     };
-  }
+    return html;
+  };
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -54,4 +51,33 @@ $(function(){
       alert('error');
     })
   });
+
+  var reloadMessages = function(){
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: "GET",
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages){
+      if (messages.length !== 0){
+        var insartHTML = '';
+        $.each(messages, function(i,message){
+          insartHTML += buildHTML(message)
+        });
+        $('.mainchat__messagelist').append(insartHTML);
+        $('.mainchat__messagelist').animate({ scrollTop: $('.mainchat__messagelist')[0].scrollHeight});
+        $('form')[0].reset();
+        $('.submit__btn').prop('disabled', false);
+      }
+    })
+    .fail(function(){
+      console.log('error');
+    });
+  };
+
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
